01eff87e0f62da7e3d09b5112d02aeb008ba3340
张吉锴 <jikaizhang@dj.cn>
2019-06-13 20:34:17 +0800

    PriorityBlockingQueue

--- 003-PriorityBlockingQueue/src/main/java/com/stzhangjk/demo/concurrent/priority/blocking/queue/PriorityBlockingQueueDemo.java
+++ 003-PriorityBlockingQueue/src/main/java/com/stzhangjk/demo/concurrent/priority/blocking/queue/PriorityBlockingQueueDemo.java
@@ -1,13 +1,14 @@
-package com.stzhangjk.demo.concurrent;
+package com.stzhangjk.demo.concurrent.priority.blocking.queue;
 
 import java.util.Arrays;
 import java.util.Iterator;
-import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.PriorityBlockingQueue;
 
-public class QueueTest {
+public class PriorityBlockingQueueDemo {
     public static void main(String[] args){
-        Queue queue = new PriorityBlockingQueue<Integer>();
+        /*PriorityBlockingQueue*/
+        BlockingQueue<Integer> queue = new PriorityBlockingQueue<>();
         queue.offer(1);
         queue.offer(3);
         queue.offer(2);
@@ -22,5 +23,6 @@ public class QueueTest {
         Object[] arr = queue.toArray();
         Arrays.sort(arr);
         System.out.println(Arrays.toString(arr));      //sort后才有序
+
     }
 }
